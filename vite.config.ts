fc3b172db1b344897d7bbcec4a7b05e881d9ab34
amrit4322 <aj044223@gmail.com>
2025-12-20 17:27:16 +1100

    Updated Hosting link

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  // base: "/golden-jago/",
+  base: "/golden-jago/",
   build: { sourcemap: true },
   resolve: {
     alias: {
