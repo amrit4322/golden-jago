7d5991bc88d5d73975c67972051d4bc0f8b7a136
amrit4322 <aj044223@gmail.com>
2025-12-21 23:40:39 +1100

    Updated homepage link

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="golden-jago">
+      <BrowserRouter >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
